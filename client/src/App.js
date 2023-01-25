8e428d85966cc4a9a9bfefc12e2766dfb38825f9
Tahlia <tbolden13@gmail.com>
2023-01-25 13:45:12 -0800

    app js file edited for apollo

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,51 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+ // cache: new InMemoryCache(),
+});
+
+const authLInk = setContext((_, { header}) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    header: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLInk.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
         <Routes>
           <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
+            path='/'  element={<SearchBooks />} />
           <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
+            path='/saved'  element={<SavedBooks />} />
           <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
+            path='*' element={<h1 className='display-2'>Wrong page!</h1>} />
         </Routes>
       </>
     </Router>
+  </ApolloProvider>
   );
 }
 
