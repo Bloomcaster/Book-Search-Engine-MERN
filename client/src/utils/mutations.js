e58d7415c69663b75002cc5ff865fc01ba214b03
Tahlia <tbolden13@gmail.com>
2023-01-25 13:13:46 -0800

    added removebook code to mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -56,3 +56,21 @@ export const SAVE_BOOK = gql`
         }
     }
 `;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
