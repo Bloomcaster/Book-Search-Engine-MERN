eec29332e3b55438999c67b44281961d5544f812
Tahlia <tbolden13@gmail.com>
2023-01-25 13:10:57 -0800

    mutations login/add user and savebook code added

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,48 @@ export const LOGIN_USER = gql`
         }
     }
 `;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation savebook(
+        $bookId: String
+        $authors: [String]
+        $description: String
+        $title: String
+        $image: String
+        $link: String
+    ) {
+        saveBook(
+            bookId: $bookId
+            authors: $authors
+            description: $description
+            titleL $title
+            image: $image
+            link: $link
+        ) {
+           _id
+           username
+           email
+           bookCount
+           savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+           } 
+        }
+    }
+`;
