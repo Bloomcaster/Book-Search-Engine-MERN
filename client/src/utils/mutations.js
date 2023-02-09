1f1c75649702af07672cdfd6562e8421a0ab463e
Tahlia <tbolden13@gmail.com>
2023-02-08 21:38:49 -0800

    fixed compiling errorsgit add .

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
     mutation login($email: String, $password: String) {
-        login(email, password: $password) {
+        login(email: $email, password: $password) {
             token
             user {
                 _id
@@ -26,19 +26,19 @@ export const ADD_USER = gql`
 
 export const SAVE_BOOK = gql`
     mutation savebook(
-        $bookId: String
-        $authors: [String]
-        $description: String
-        $title: String
-        $image: String
+        $bookId: String,
+        $authors: [String],
+        $description: String,
+        $title: String,
+        $image: String,
         $link: String
     ) {
         saveBook(
-            bookId: $bookId
-            authors: $authors
-            description: $description
-            titleL $title
-            image: $image
+            bookId: $bookId,
+            authors: $authors,
+            description: $description,
+            title: $title,
+            image: $image,
             link: $link
         ) {
            _id
